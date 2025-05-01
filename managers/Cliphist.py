22acda0329a3f0971292dd8dda382dd5759d767d
FredrikLohmann <47090598+FredrikLohmann@users.noreply.github.com>
2025-05-01 21:34:57 +0200

    Update Cliphist.py

--- managers/Cliphist.py
+++ managers/Cliphist.py
@@ -23,7 +23,7 @@ def start():
     subprocess.Popen([paste_agent, "--watch", client ,'store'])
 
 def add(text):
-    subprocess.call([copy_agent, re.sub(r'(^\s*\d+\s*', '' , text)])
+    subprocess.call([copy_agent, re.sub(r'^\s*\d+\s*', '', text)])
 
 def get_history():
     return exec_get(client, 'list').splitlines()
