4c643346988f57d6a9fe9f24ed2c16cbd89b95b7
FredrikLohmann <47090598+FredrikLohmann@users.noreply.github.com>
2025-05-01 20:27:07 +0200

    Update Cliphist.py

--- managers/Cliphist.py
+++ managers/Cliphist.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import json
 from shutil import which
-from lib import exec_get, pid_of
+from lib import exec_get, pid_of, show_message
 
 name = 'Cliphist'
 client = 'cliphist'
@@ -25,6 +25,11 @@ def add(text):
     subprocess.call([copy_agent, text])
 
 def get_history():
+            show_message(
+            'ulauncher-clipboard error',
+            "get history",
+            'dialog-error'
+        )
     val=json.loads(exec_get(client, 'list'))
     return val
 
